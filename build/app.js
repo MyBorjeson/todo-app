e336c272841c77539abd37e9dbc4f5495b45b268
My Boörjeson <myborjeson@gmail.com>
2015-10-25 10:59:16 +0100

    First function rendering a list item

--- build/app.js
+++ build/app.js
@@ -1,1 +1,15 @@
 alert("Hello World!");
+
+var TodoApp = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ul>
+					<li>Here is my first todo item!</li>
+				</ul>
+			</div>
+			);
+	}
+});
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
